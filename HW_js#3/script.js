57b743ff1e4d069ffacd83b30f50c5fa15bb67d9
Tetiana Okhotiuk <tkhedyk@gmail.com>
2017-05-23 18:10:13 +0300

    HW_js#3-4

---
+++ HW_js#3/script.js
@@ -0,0 +1,195 @@
+var div = document.createElement('div');
+div.className = "wrapper";
+document.body.appendChild(div);
+
+var h3 = document.createElement('h3');
+var wrapper = document.querySelector(".wrapper");
+wrapper.appendChild(h3);
+h3.innerHTML = "Programming test"
+
+var ol = document.createElement("ol");
+ol.className = "questions";
+wrapper.appendChild(ol);
+
+var li = document.createElement("li");
+li.className = "variant1";
+var questions = document.querySelector(".questions");
+questions.appendChild(li);
+li.innerHTML = "Question#1"
+
+var li = document.createElement("li");
+li.className = "variant2";
+var questions = document.querySelector(".questions");
+questions.appendChild(li);
+li.innerHTML = "Question#2"
+
+var li = document.createElement("li");
+li.className = "variant3";
+var questions = document.querySelector(".questions");
+questions.appendChild(li);
+li.innerHTML = "Question#3"
+
+var button = document.createElement("button");
+button.className = "btn-default";
+button.classList.add("button");
+wrapper.appendChild(button);
+button.innerHTML = "Check answers"
+
+var ul = document.createElement('ul');
+ul.className = "link";
+var variant1 = document.querySelector(".variant1");
+variant1.appendChild(ul);
+
+var li = document.createElement('li');
+li.className = "checkin";
+var link = document.querySelector('.link');
+link.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin = document.querySelector(".checkin");
+checkin.appendChild(input);
+
+var label = document.createElement("label");
+label.className = "checking";
+var checkin = document.querySelector(".checkin");
+checkin.appendChild(label);
+label.innerHTML = "variant of question #1"
+
+var li = document.createElement('li');
+li.className = "checkin2";
+var link = document.querySelector('.link');
+link.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin2 = document.querySelector(".checkin2");
+checkin2.appendChild(input);
+
+var label = document.createElement("label");
+label.className = "checking";
+var checkin2 = document.querySelector(".checkin2");
+checkin2.appendChild(label);
+label.innerHTML = "variant of question #2"
+
+var li = document.createElement('li');
+li.className = "checkin3";
+var link = document.querySelector('.link');
+link.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin3 = document.querySelector(".checkin3");
+checkin3.appendChild(input);
+
+var label = document.createElement("label");
+label.className = "checking";
+var checkin3 = document.querySelector(".checkin3");
+checkin3.appendChild(label);
+label.innerHTML = "variant of question #3"
+
+var ul = document.createElement('ul');
+ul.className = "link2";
+var variant2 = document.querySelector(".variant2");
+variant2.appendChild(ul);
+
+var li = document.createElement('li');
+li.className = "checkin4";
+var link2 = document.querySelector('.link2');
+link2.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin4 = document.querySelector(".checkin4");
+checkin4.appendChild(input);
+
+var label = document.createElement("label");
+var checkin4 = document.querySelector(".checkin4");
+checkin4.appendChild(label);
+label.innerHTML = "variant of question #1"
+
+var li = document.createElement('li');
+li.className = "checkin5";
+var link2 = document.querySelector('.link2');
+link2.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin5 = document.querySelector(".checkin5");
+checkin5.appendChild(input);
+
+var label = document.createElement("label");
+var checkin5 = document.querySelector(".checkin5");
+checkin5.appendChild(label);
+label.innerHTML = "variant of question #2"
+
+var li = document.createElement('li');
+li.className = "checkin6";
+var link2 = document.querySelector('.link2');
+link2.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin6 = document.querySelector(".checkin6");
+checkin6.appendChild(input);
+
+var label = document.createElement("label");
+var checkin6 = document.querySelector(".checkin6");
+checkin6.appendChild(label);
+label.innerHTML = "variant of question #3"
+
+var ul = document.createElement('ul');
+ul.className = "link3";
+var variant3 = document.querySelector(".variant3");
+variant3.appendChild(ul);
+
+var li = document.createElement('li');
+li.className = "checkin7";
+var link3 = document.querySelector('.link3');
+link3.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin7 = document.querySelector(".checkin7");
+checkin7.appendChild(input);
+
+var label = document.createElement("label");
+var checkin7 = document.querySelector(".checkin7");
+checkin7.appendChild(label);
+label.innerHTML = "variant of question #1"
+
+var li = document.createElement('li');
+li.className = "checkin8";
+var link3 = document.querySelector('.link3');
+link3.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin8 = document.querySelector(".checkin8");
+checkin8.appendChild(input);
+
+var label = document.createElement("label");
+var checkin8 = document.querySelector(".checkin8");
+checkin8.appendChild(label);
+label.innerHTML = "variant of question #2"
+
+var li = document.createElement('li');
+li.className = "checkin9";
+var link3 = document.querySelector('.link3');
+link3.appendChild(li);
+
+var input = document.createElement('input');
+input.setAttribute("type", "checkbox");
+var checkin9 = document.querySelector(".checkin9");
+checkin9.appendChild(input);
+
+var label = document.createElement("label");
+var checkin9 = document.querySelector(".checkin9");
+checkin9.appendChild(label);
+label.innerHTML = "variant of question #3"
+
+
+
+
+
+
