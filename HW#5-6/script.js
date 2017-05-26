6e58d0c13fc97e049b6cee4efd213da5c80f7879
Tetiana Okhotiuk <tkhedyk@gmail.com>
2017-05-26 14:07:18 +0300

    updated hw#5-6

--- HW#5-6/script.js
+++ HW#5-6/script.js
@@ -13,6 +13,7 @@ var t = 0;
  document.getElementById('timer').innerHTML = 0;
 
  function start() {
+ 	btn.innerHTML = 'PAUSE'
  	timer = new Date().getTime();
  	var interval = setInterval(function(){
  		var t = new Date().getTime() - timer - seconds * 1000
@@ -22,19 +23,34 @@ var t = 0;
 ":" + minutes + ":" + seconds;
  		if (t>999) {seconds++}
  			if (seconds>59) {minutes++; 
- 				seconds = (seconds - (minutes * 60));}
+ 				seconds = 0;}
  			if (minutes>59) {hours++;
- 				minutes = minutes - hours*60;}
-   
+ 				minutes = 0;}
 
  	},1)
+
+ 	function pause() {
+ 			timer = new Date().getTime();
+ 	var t = timer;
+ 		document.getElementById('timer').innerHTML = t;
+ 		  document.getElementById('time').innerHTML = hours + 
+":" + minutes + ":" + seconds;
+ 	clearInterval(interval);
+ 
  }
+ 	btn.addEventListener('click', pause);
+ }
+
+ 
 
 
 var btn = document.createElement('button');
 document.body.appendChild(btn);
 btn.appendChild(document.createTextNode('START'));
 btn.addEventListener('click', start);
+
+
+
 
 
  // if (hours < 10) {hours = "0" + hours;}
