9950e132f5aa130c58cf0d73b032edc557b9f3d7
Tetiana Okhotiuk <tkhedyk@gmail.com>
2017-05-26 12:07:44 +0300

    updated HW_js#5-6

--- HW#5-6/script.js
+++ HW#5-6/script.js
@@ -6,32 +6,51 @@ var timer = 0;
 var t = 0;
 
 
-document.getElementById('time').innerHTML = hours + 
+
+   document.getElementById('time').innerHTML = hours + 
 ":" + minutes + ":" + seconds;
 
+ document.getElementById('timer').innerHTML = 0;
+
+ function start() {
+ 	timer = new Date().getTime();
+ 	var interval = setInterval(function(){
+ 		var t = new Date().getTime() - timer - seconds * 1000
+ 		 - minutes * 60 * 1000 - hours * 360 * 1000;
+ 		document.getElementById('timer').innerHTML = t;
+ 		  document.getElementById('time').innerHTML = hours + 
+":" + minutes + ":" + seconds;
+ 		if (t>999) {seconds++}
+ 			if (seconds>59) {minutes++; 
+ 				seconds = (seconds - (minutes * 60));}
+ 			if (minutes>59) {hours++;
+ 				minutes = minutes - hours*60;}
+   
+
+ 	},1)
+ }
 
 
-document.getElementById('start').onclick = 
-function(){timer = new Date().getTime()}
-document.getElementById('stop').onclick = 
-function(){timer = 0}
+var btn = document.createElement('button');
+document.body.appendChild(btn);
+btn.appendChild(document.createTextNode('START'));
+btn.addEventListener('click', start);
 
-var interval = setInterval(function(){
-    if (timer==0) return;
-	var t = (new Date().getTime()-timer - seconds*1000); 
-    document.getElementById('timer').innerHTML = 
-    (new Date().getTime()-timer- seconds*1000)
-},1);
 
-if (t>999) {++seconds;
-}
+ // if (hours < 10) {hours = "0" + hours;}
+ //    if (minutes < 10) {minutes = "0" + minutes;}
+ //    if (seconds < 10) {seconds = "0" + seconds;}
 
 
 
 
 
-// //     if (hours < 10) hours = "0" + hours;
-//     if (minutes < 10) minutes = "0" + minutes;
-//     if (seconds < 10) seconds = "0" + seconds;
-//   }
+
+
+
+
+
+
+
+
 
