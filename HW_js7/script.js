5cb2fa7cc5e7d8302486f9c8ba0be1921bcd825d
Tetiana Okhotiuk <tkhedyk@gmail.com>
2017-08-02 18:53:58 +0300

    corrections

--- HW_js7/script.js
+++ HW_js7/script.js
@@ -3,29 +3,23 @@ $(document).ready(function(){
 
     $(".btn").click(function(){
         var id = $(this).attr('id');
+            $('.btn').removeClass('btn-primary');
+            $(".text").hide();
+            $(this).addClass("btn-primary"); 
 
         switch(id) {
 
-            case 'btn1':
-                $('.btn').removeClass('btn-primary');
-                $(".text").hide();
-                $(".first").show();
-                $(this).addClass("btn-primary"); 
+            case 'btn1':                
+                $(".first").show();                
                 break;
 
-            case 'btn2':
-                $('.btn').removeClass('btn-primary');
-                $(".text").hide();
-                $(".second").show();
-                $(this).addClass("btn-primary"); 
+            case 'btn2':                
+                $(".second").show();               
                 break;
 
-            case 'btn3':
-                $('.btn').removeClass('btn-primary');
-                $(".text").hide();
+            case 'btn3':                
                 $(".third").show(); 
-                $(".fourth").show();
-                $(this).addClass("btn-primary");
+                $(".fourth").show();               
                 break;
         }
     });
