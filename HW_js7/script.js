aea265425112ab65838787bc692db5b2fc59bc0d
Tetiana Okhotiuk <tkhedyk@gmail.com>
2017-08-02 18:25:41 +0300

    corrected HW_js7

--- HW_js7/script.js
+++ HW_js7/script.js
@@ -1,37 +1,33 @@
 
 $(document).ready(function(){
-    $(".btn1").click(function(){
-        $(".first").show(1);
-        $(".second").hide(1);
-        $(".third").hide(1);
-        $(".fourth").hide(1);
-        $(this).addClass("btn-primary");
-        $(".btn2").removeClass('btn-primary');
-        $(".btn3").removeClass('btn-primary');
-    });
-});
 
-$(document).ready(function(){
-    $(".btn2").click(function(){
-        $(".second").show(1);
-        $(".first").hide(1);
-        $(".third").hide(1);
-        $(".fourth").hide(1);
-        $(this).addClass("btn-primary");
-        $(".btn1").removeClass('btn-primary');
-        $(".btn3").removeClass('btn-primary');
-    });
-});
+    $(".btn").click(function(){
+        var id = $(this).attr('id');
 
-$(document).ready(function(){
-    $(".btn3").click(function(){
-        $(".third").show(1);
-        $(".fourth").show(1);
-        $(".second").hide(1);
-        $(".first").hide(1);
-        $(this).addClass("btn-primary");
-        $(".btn1").removeClass('btn-primary');
-        $(".btn2").removeClass('btn-primary');
+        switch(id) {
+
+            case 'btn1':
+                $('.btn').removeClass('btn-primary');
+                $(".text").hide();
+                $(".first").show();
+                $(this).addClass("btn-primary"); 
+                break;
+
+            case 'btn2':
+                $('.btn').removeClass('btn-primary');
+                $(".text").hide();
+                $(".second").show();
+                $(this).addClass("btn-primary"); 
+                break;
+
+            case 'btn3':
+                $('.btn').removeClass('btn-primary');
+                $(".text").hide();
+                $(".third").show(); 
+                $(".fourth").show();
+                $(this).addClass("btn-primary");
+                break;
+        }
     });
 });
 
