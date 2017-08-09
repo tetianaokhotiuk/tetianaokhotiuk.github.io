a654e74669033d6d603941163ec053dca68266ba
Tetiana Okhotiuk <tkhedyk@gmail.com>
2017-08-09 22:39:37 +0300

    correction

--- HW_js7/script.js
+++ HW_js7/script.js
@@ -3,25 +3,11 @@ $(document).ready(function(){
 
     $(".btn").click(function(){
         var id = $(this).attr('id');
+        var lastNum = id.slice(-1);
             $('.btn').removeClass('btn-primary');
             $(".text").hide();
+            $(".text"+lastNum).show();
             $(this).addClass("btn-primary"); 
-
-        switch(id) {
-
-            case 'btn1':                
-                $(".first").show();                
-                break;
-
-            case 'btn2':                
-                $(".second").show();               
-                break;
-
-            case 'btn3':                
-                $(".third").show(); 
-                $(".fourth").show();               
-                break;
-        }
     });
 });
 
